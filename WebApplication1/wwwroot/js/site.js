b31727ed8f0ab633cce65773c97435a6d2cdb75e
Ahmedjanov Shahzod <80168982+Ahmedjanov@users.noreply.github.com>
2022-04-10 23:28:14 +0600

    create audio player without connecting to data base

---
+++ WebApplication1/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+
+function playAudio(src) {
+	//document.write(path);
+
+	var sound = document.createElement('audio');
+	sound.controls = 'controls';
+	sound.src = src;
+	sound.type = 'audio/mpeg';
+	sound.autoplay = 'autoplay';
+	$('#Player').empty();
+	document.getElementById('Player').appendChild(sound);
+	document.getElementById('Player').style.visibility = 'visible';
+}
