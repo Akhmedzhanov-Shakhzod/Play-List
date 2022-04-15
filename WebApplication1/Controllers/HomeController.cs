bd37c8ed2e579cadb875fc8905eba7e67110cf5b
Ahmedjanov Shahzod <80168982+Ahmedjanov@users.noreply.github.com>
2022-04-16 03:29:22 +0600

    realize searching mechanism

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -45,11 +45,14 @@ namespace WebApplication1.Controllers
              
                 if (userindb == null)
                 {
+                    var tracks = from u in _context.tracks
+                                 select u;
+                    tracks = tracks.OrderByDescending(u => u);
                     user.UserAccessLevel = Helper.ValidateAdmin(user.UserAccessLevel) ? "admin" : "user";
                     _context.Add(user);
                     await _context.SaveChangesAsync();
                     Helper.user = user;
-                    return View("Index", _context);
+                    return View("Index", tracks);
                 }
                 else
                 {
@@ -80,16 +83,59 @@ namespace WebApplication1.Controllers
                 {
                     if(userindb.Password != user.Password) return View("Login", Helper.Errors.PassworInCorrect);
 
+                    var tracks = from u in _context.tracks
+                                 select u;
+                    tracks = tracks.OrderByDescending(u => u);
+
                     user.UserAccessLevel = userindb.UserAccessLevel;
                     Helper.user = user;
-                    return View("Index", _context);
+                    return View("Index", tracks);
                 }
             }
             return RedirectToAction("Login");
         }
+
+        public IActionResult OrderBy(string searchId)
+        {
+
+            var tracks = from u in _context.tracks
+                        select u;
+
+            switch (searchId)
+            {
+                case "1":
+                    tracks = tracks.OrderBy(u => u.TrackName);
+                    break;
+                case "2":
+                    tracks = tracks.OrderBy(u => u.Artist);
+                    break ;
+                case "3":
+                    tracks = tracks.OrderBy(u => u.Listens);
+                    break;
+            }
+
+            return View("Index", tracks);
+        }
+
+        public IActionResult Search(string searchString)
+        {
+
+            var tracks = from u in _context.tracks
+                         select u;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                tracks = tracks.Where(s => s.Artist.Contains(searchString) || s.TrackName.Contains(searchString));
+            }
+
+            return View("Index", tracks);
+        }
         public IActionResult Index()
         {
-            return View("Index",_context);
+            var tracks = from u in _context.tracks
+                         select u;
+            tracks = tracks.OrderByDescending(u => u);
+            return View("Index",tracks);
         }
 
         public IActionResult Privacy()
--- WebApplication1/wwwroot/js/site.js
+++ WebApplication1/wwwroot/js/site.js
@@ -17,3 +17,41 @@ function playAudio(src) {
 	document.getElementById('Player').appendChild(sound);
 	document.getElementById('Player').style.visibility = 'visible';
 }
+
+
+
+
+/*
+
+document.querySelector('#elastic').oninput = function () {
+	let val = this.value.trim();
+
+	let elastinItmes = document.querySelectorAll('.elastic');
+
+	if (val != '') {
+		elastinItmes.forEach(function (elem) {
+
+			if (elem.innerText.search(val) == -1) {
+				elem.classList.add('hide');
+			}
+			else {
+				elem.classList.remove('hide');
+
+				let str = elem.innerHTML;
+				elem.innerHTML = insertMark(str, elem.innerText.search(val), val.length)
+			}
+		})
+	}
+	else {
+		elastinItmes.forEach(function (elem) {
+			elem.classList.remove('hide');
+		})
+	}
+}
+
+function insertMark(string,pos,len) {
+
+	return string.slice(0, pos) + '<mark>' + string.slice(pos, pos + len) + '</mark>' + string.slice(pos + len, string.length);
+}
+* /
+
