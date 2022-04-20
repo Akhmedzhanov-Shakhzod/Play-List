382841599e86a7e5e3bf423e3ca86a445a73858c
Ahmedjanov Shahzod <80168982+Ahmedjanov@users.noreply.github.com>
2022-04-21 02:46:00 +0600

    add page to show details and edit playlist

--- WebApplication1/Controllers/PlayListController.cs
+++ WebApplication1/Controllers/PlayListController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApplication1.DataLayer;
 using WebApplication1.Models;
 
@@ -32,12 +33,30 @@ namespace WebApplication1.Controllers
             return tracks;
         }
 
+        public (PlayList,IQueryable<Tracks>) LoadEditor(int id)
+        {
+            var playlist = (from p in _context.playLists
+                            where (p.Id == id)
+                            select p).ToList();
+
+            var tracks = from tp in _context.tracksInPlayList
+                         join t in _context.tracks
+                         on tp.TrackId equals t.TrackId
+                         join p in _context.playLists
+                         on tp.PlayListId equals p.Id
+                         where p.Author == Helper.user.UserName && p.Id == id
+                         select t;
+
+            var tracksInPlayList = (playlist[0], tracks);
+            return tracksInPlayList;
+        }
+
         public IActionResult Index()
         {
             return View("Index",Load());
         }
         
-        public async Task<IActionResult> Create(IFormFile Image, List<int> Tracks)
+        public async Task<IActionResult> Create(List<IFormFile> Image, List<int> Tracks)
         {
             string name = Request.Form["Name"];
             Helper.isPlaylistExist = false;
@@ -57,15 +76,15 @@ namespace WebApplication1.Controllers
                 Name = name,
                 Author = Helper.user.UserName
             };
-            if (Image != null)
+            if (Image.Count > 0)
             {
-                string pathpicture = Path.Combine("wwwroot/files/pictures", Image.FileName);
+                string pathpicture = Path.Combine("wwwroot/files/pictures", Image[0].FileName);
 
                 using (var fileStream = new FileStream(pathpicture, FileMode.Create))
                 {
-                    await Image.CopyToAsync(fileStream);
+                    await Image[0].CopyToAsync(fileStream);
                 }
-                pathpicture = Path.Combine("/files/pictures", Image.FileName);
+                pathpicture = Path.Combine("/files/pictures", Image[0].FileName);
                 playList.Picture = pathpicture;
             }
             else
@@ -90,6 +109,83 @@ namespace WebApplication1.Controllers
             return View("Index", Load());
         }
 
+        public IActionResult EditPage(int id)
+        {
+            Helper.player = "";
 
+            return View("Edit", LoadEditor(id));
+        }
+
+        public async Task<IActionResult> Edit(int playlistid)
+        {
+            string name = Request.Form["Name"];
+            Helper.isPlaylistExist = false;
+
+            ////
+            ///
+            var playlistindb = _context.playLists.FirstOrDefault(p => p.Name == name && p.Author == Helper.user.UserName);
+            if (playlistindb != null)
+            {
+                Helper.isPlaylistExist = true;
+                return View("Edit", LoadEditor(playlistid));
+            }
+            ////
+            ///
+
+            var playlist = await _context.playLists.FirstOrDefaultAsync(p => p.Id == playlistid);
+
+            playlist.Name = name;
+
+            _context.playLists.Update(playlist);
+            await _context.SaveChangesAsync();
+
+            return View("Edit", LoadEditor(playlistid));
+        }
+        public async Task<IActionResult> Delete(int id,int playlistid)
+        {
+
+            var track = _context.tracksInPlayList.FirstOrDefault(t => t.PlayListId == playlistid && t.TrackId == id);
+
+            if (track != null)
+            {
+                _context.tracksInPlayList.Remove(track);
+                await _context.SaveChangesAsync();
+            }
+
+            return View("Edit", LoadEditor(playlistid));
+        }
+        public async Task<IActionResult> DeletePlayList(int playlistid)
+        {
+
+            var track = _context.playLists.FirstOrDefault(p => p.Id == playlistid);
+
+            _context.playLists.Remove(track);
+            await _context.SaveChangesAsync();
+
+            return View("Index",Load());
+        }
+        public async Task<IActionResult> Player(string scr, int trackid,int playlistid)
+        {
+            //var result = "<source src = \"";
+            //result += scr;
+            //result += "\" type = \"audio/mpeg\">";
+            Helper.player = scr;
+
+            var track = await _context.tracks.FindAsync(trackid);
+
+            track.Listens += 1;
+
+            try
+            {
+                _context.tracks.Update(track);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+
+            return View("Edit", LoadEditor(playlistid));
+        }
     }
 }
